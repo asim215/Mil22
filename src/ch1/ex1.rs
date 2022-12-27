3637b0d1cbd76eff673e65c425025bcd0dd3253d
Anton Simonov <asimonov215@gmail.com>
2022-12-27 08:51:38 +0300

    Exercises from Chapter 1

--- src/ch1/ex1.rs
+++ src/ch1/ex1.rs
@@ -1,3 +1,20 @@
+// Experiments with code
+
 fn main() {
-    print!("Hello World");
+    // print!("Hello World");
+    println!("{}, {}!", "Hello", "world");
+    print!("First line\nSecond line\nThird line\n");
+    println!();
+
+    // Printing Integer Numbers
+    // Number as literal string
+    println!("My number: 140");
+    // Placeholder with literal string argument
+    println!("My number: {}", "140");
+    // Placeholder with literal integer number
+    println!("My number: {}", 140);
+    //
+    println!("My number: {}", 000145);
+    // Mixed arguments
+    println!("{}: {}", "My number", 147);
 }
